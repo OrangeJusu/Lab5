789cf19c375028ab9003f48d9b5ff21c297b19ea
Aaron Knoll <91743185+OrangeJusu@users.noreply.github.com>
2023-04-03 14:33:13 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -5,8 +5,43 @@ using namespace std;
 
 /*For the Lab instead of making two seperate classes for the nodes
   and having five files I thought I would slim it down and make the
-  nodes a structure in the tree class and only having three files.*/
+  nodes a structure in the tree class and only having three files.
+  
+  Also for the print tree function its facing towards the right like this:
+        yahoo.com
+     google.com
+        facebook.com
+                azap.com
+            apple.com
+                amazon.com*/
 
 int main() {
-	
+    BLT tree;
+    tree.Add("google.com");
+    tree.Add("facebook.com");
+    tree.Add("apple.com");
+    tree.Add("amazon.com");
+    tree.Add("yahoo.com");
+    tree.Add("azap.com");
+
+    tree.Print();
+    cout << endl;
+
+    tree.RPrint();
+    cout << endl;
+
+    tree.TPrint();
+    cout << endl;
+
+    tree.Remove("facebook.com");
+    tree.Remove("apple.com");
+
+    tree.Print();
+    cout << endl;
+
+    tree.RPrint();
+    cout << endl;
+
+    tree.TPrint();
+    cout << endl;
 }
